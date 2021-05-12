7bdaf27563852aede7b7ea8eddb03d148502403c
unknown <dankosimic@gmail.com>
2021-05-12 17:52:44 +0200

    drugi commit

--- FirmaJP24.sql
+++ FirmaJP24.sql
@@ -1,3 +1,26 @@
-drop database if exists FirmaJP24;
-create database FirmaJP24;
-use FirmaJP24;
+drop database if exists firmaJP24;
+create database firmaJP24;
+use firmaJP24;
+
+create table projekt (
+	id int primary key auto_increment not null,
+	naziv varchar (50) not null,
+	cijena decimal (18,2) not null
+);
+
+create table programer (
+	id int primary key auto_increment not null,
+	ime varchar (50) not null,
+	prezime varchar (50) not null,
+	datum_rodjenja datetime,
+	placa decimal (18,2) not null
+);
+
+create table sudjeluje (
+	projekt int not null,
+	programer int not null,
+	datum_pocetka datetime,
+	datum_kraja datetime
+);
+
+alter table sudjeluje
